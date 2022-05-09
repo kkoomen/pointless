8dd89c73d4f2d066c8eed03399f223c7df0fcc6d
James Zow <jameszow@163.com>
2022-05-10 01:50:36 +0800

    use const and return optimization

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,15 +1,21 @@
 use std::io::{Write, Read};
 use std::fs::File;
 
+const BUFFER_SIZE: usize = 4096;
+const Q: u32 = 11;
+const LGWIN: u32 = 22;
+
 pub fn compress(filename: &str, contents: &str) -> bool {
-    let mut writer = brotli::CompressorWriter::new(File::create(filename).unwrap(), 4096, 11, 22);
-    write!(&mut writer, "{}", contents).expect("Could not compress contents");
-    return true;
+    let writer = brotli::CompressorWriter::new(File::create(filename).unwrap(), BUFFER_SIZE, Q, LGWIN);
+    write!(writer, "{}", contents).expect("Could not compress contents");
+    
+    true
 }
 
 pub fn decompress(filename: &str) -> String {
-    let mut reader = brotli::Decompressor::new(File::open(filename).unwrap(), 4096);
+    let reader = brotli::Decompressor::new(File::open(filename).unwrap(), BUFFER_SIZE);
     let mut data = String::new();
     reader.read_to_string(&mut data).expect("Unable to decompress contents");
-    return data;
+    
+    data
 }
